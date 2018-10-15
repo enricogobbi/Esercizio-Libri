55569abb36cea54d25d047f787d138eab9807689
enricogobbi <enrico.gobbi.itiscesena@gmail.com>
2018-10-15 10:39:49 +0200

    Caricamento file XML

---
+++ Esercizio Libri-Gobbi Iaconis/Esercizio Libri-Gobbi Iaconis/DocumentoXML.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esercizio_Libri_Gobbi_Iaconis
+{
+    class DocumentoXML
+    {
+    }
+}
